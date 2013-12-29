76c35b5145ba832e1a8879ae18f87d35dae5cdf8
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2013-12-29 13:45:33 +0100

    added functionality

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,48 @@
 module Main where
+
+import System.Console.ANSI
+
 main :: IO ()
 main = do
     putStrLn "You fight with the strength of many men sir Knight..."
     putStrLn "You have proved yourself worthy; will you join me?"
     putStrLn "You make me sad. So be it. Come, Patsy."
+    intro
+    end
+
+
+
+colorPutStr :: Color -> String -> IO ()
+colorPutStr color str = do
+    setSGR  [ SetColor Foreground Dull color
+            , SetConsoleIntensity NormalIntensity
+            ]
+    putStr str
+    setSGR []
+
+
+bk :: String -> IO ()
+bk str = colorPutStr Green ("Bridgekeeper: " ++ str ++ "\n")
+bkn :: String -> IO ()
+bkn str = colorPutStr Green ("Bridgekeeper: " ++ str)
+you :: String -> IO ()
+you str = colorPutStr Yellow ("Bridgekeeper: " ++ str ++ "\n")
+
+intro :: IO ()
+intro = do
+    bk "Stop!"
+    bk "Who would cross the Bridge of Death"
+    bk "must answer me these questions three,"
+    bk "ere the other side he see."
+    you "Ask me the questions, bridgekeeper, I am not afraid.\n"
+
+end :: IO ()
+end = do
+    putStrLn "\n\n"
+    bk "What... is the air-speed velocity of an unladen swallow?"
+    you "What do you mean? An African or European swallow?"
+    bk "Huh? I... I don't know that."
+    putStrLn "[the bridgekeeper is thrown over]"
+    bk "Auuuuuuuuuuuugh"
+    putStrLn "Sir Bedevere: How do you know so much about swallows?"
+    you "Well, you have to know these things when you're a king, you know."
